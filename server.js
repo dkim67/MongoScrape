cd4b28908465086f3248a34898366d001ceceb09
Daniel Kim <danielkim@Daniels-MacBook-Air-2.local>
2018-08-16 16:24:26 -0400

    fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 
@@ -17,7 +17,7 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 
-
+app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
